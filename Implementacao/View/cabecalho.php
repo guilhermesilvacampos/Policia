2f075f0e7fef9b426190a558c19a34689bb754b6
unknown <guilherme.silva.c@hotmail.com>
2018-10-01 16:25:49 -0300

    conecta e caecalho

---
+++ Implementacao/View/cabecalho.php
@@ -0,0 +1,48 @@
+
+<?php 
+
+function carregaClasse($nomeDaClasse){
+	require_once("class/".$nomeDaClasse.".php");
+}
+
+spl_autoload_register("carregaClasse");
+
+error_reporting(E_ALL ^ E_NOTICE);
+require_once("mostra-alerta.php");
+require_once("conecta.php");
+?>
+<html>
+
+<head>
+	<meta charset= "utf-8">
+	<title>Minha Loja</title>
+	<link href= css/bootstrap.css rel="stylesheet">
+	<link href= "css/loja.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class= "container">
+			<div class = "navbar-header">
+				<a class= "navbar-brand" href="index.php">Minha Loja</a>
+			</div>
+				<div>
+				<ul class = " nav navbar-nav">
+				<li><a href="produto-formulario.php"> Adicionar Produto </a> </li>
+				<li><a href = "produto-lista.php"> Lista de Produtos</a></li>
+				<li><a href="contato.php"> Contato </a></li>
+				</ul>
+				</div>
+			
+			
+		</div>
+	</div>
+
+
+	<div class="container">
+ 		<div class="principal">
+		
+<?php
+mostraAlerta("danger");
+mostraAlerta("success");
+?>
