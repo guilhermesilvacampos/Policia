c69aef661ec6647b30bd959e2088f3d29c35bc09
GUILHERME SILVA CAMPOS <guilherme.s.campos@dominio.ufms.br>
2018-10-30 11:54:37 -0400

    modificações em cadastrarCidadao

--- Implementacao/View/cadastrarCidadao.php
+++ Implementacao/View/cadastrarCidadao.php
@@ -12,14 +12,75 @@ require_once("padrao.php");
   <div class="form-group">
 
 
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" class="form-control"  placeholder="Digite o Nome Completo do Cidadão">
+    
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="alcunha">Alcunha</label>
+    <input type="text" id="alcunha" class="form-control"  >
   </div>
+
+  <div class="form-group">
+    <label for="rg">RG</label>
+    <input type="integer" id="rg" class="form-control"  >
+  </div>
+
+  <div class="form-group">
+    <label for="cpf">CPF</label>
+    <input type="integer" id="cpf" class="form-control"  >
+  </div>
+
+  <div class="form-group">
+    <label for="data">Data de Nascimento</label>
+    <input type="date" id="data" class="form-control"  >
+  </div>
+  <div class="form-group">
+    <label for="nacionalidade">Nacionalidade</label>
+    <select  id="nacionalidade" class="form-control">
+  <option>Default select</option>
+</select>
+  </div>
+
+  
+
+<div class="form-group">
+    <label for="estado">Estado</label>
+    <select  id="estado" class="form-control">
+  <option>Default select</option>
+</select>
+  </div>
+
+<div class="form-group">
+    <label for="cidade">Cidade</label>
+    <select  id="cidade" class="form-control">
+  <option>Default select</option>
+</select>
+  </div>
+
+  <div class="form-group">
+    <table>
+      <tr>
+        <td class="col-md-3">
+<div class="form-group">
+          <label for="rua">Rua</label>
+    <input type="text" id="rua" class="form-control"  >
+</div>
+     </td>
+    <td>
+<div class="form-group">
+      <label for="numero">Número</label>
+    <input type="text" id="numero" class="form-control"  > 
+</div>
+  </td>
+
+      </tr>
+    </table>
+
+  </div>
+  
+
+
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
